894d26846f0d77ceccfdf82c7c99d8239f6ea3a4
Hiney Team <hineycointailz@gmail.com>
2025-12-16 14:57:18 -0500

    Fixed Buffer bug

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills' // 👈 Import this
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills(), // 👈 Add this line
+  ],
 })
